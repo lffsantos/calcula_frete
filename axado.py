50a077b1f383e5d641da072210ef30863abd00c5
lucas <lffsantos@gmail.com>
2015-12-09 00:18:07 -0300

    first commit

---
+++ axado.py
@@ -0,0 +1,23 @@
+import sys
+import csv
+__author__ = 'lucas'
+
+
+def format_output():
+    pass
+
+
+def calcula_frete(tabela):
+    pass
+
+
+def calcula_prazo(tabela):
+    pass
+
+
+def read_file(file, header=True):
+    pass
+
+
+if __name__ == '__main__':
+    pass
